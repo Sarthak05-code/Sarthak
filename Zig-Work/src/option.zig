9753a241d0fcee3c5a1937929536407e96ad48f3
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-14 18:46:10 +0545

    Shift left or right operator with array and global constant

--- Zig-Work/src/option.zig
+++ Zig-Work/src/option.zig
@@ -1,14 +1,27 @@
 const std = @import("std");
 
-pub fn return_exp(x: u32, exp_level: u5) u32 {
+pub fn return_exp_right(x: u32, exp_level: u5) u32 {
     return x >> exp_level;
+}
+
+// seems global variable are possible in zig, very nice.
+const CONSTANT = 2;
+
+pub fn return_exp_left(x: u32, exp_level: u5) u32 {
+    return x << exp_level;
 }
 
 pub fn main() void {
     const array: [9]u32 = .{ 5, 2, 55, 1, 4, 2, 5, 2, 45 };
 
     for (0..array.len) |i| {
-        const number = return_exp(array[i], 2);
-        std.debug.print("The number {} is: {}\n", .{ array[i], number });
+        // zig seems to not use a typical number % 2 == 0; so i am using the prefered @mod method.
+        if (@mod(array[i], 2) == 0) {
+            const number = return_exp_left(array[i], CONSTANT);
+            std.debug.print("The number {} is {}\n", .{ array[i], number });
+        } else {
+            const number = return_exp_right(array[i], CONSTANT);
+            std.debug.print("The number {} is {}\n", .{ array[i], number });
+        }
     }
 }
