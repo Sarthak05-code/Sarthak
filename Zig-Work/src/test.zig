ac2452c6297d7d0c4f07866d20ad58357aefc7c4
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-22 18:19:55 +0545

    stuffs , ig

--- Zig-Work/src/test.zig
+++ Zig-Work/src/test.zig
@@ -2,31 +2,29 @@ const A = @import("option.zig");
 const std = @import("std");
 const built = @import("builtin");
 const os = built.os.tag;
-const hash = std.crypto.hash;
+
+const e = std.math;
 
 pub fn main() !void {
     var pos: A.Advance = .{};
-    pos.advance();
-    std.debug.print("Current pos : {} \n ", .{pos});
-
     switch (os) {
         .windows => {
             pos.reset();
             pos.scalable_advance(5);
-            std.debug.print("Since your windows, you moved 5 steps ahead , Current pos : {}\n", .{pos});
+            std.debug.print("Since your windows, you moved 5 steps ahead, Current pos : {}\n", .{pos.current_pos});
         },
         .linux => {
             pos.reset();
             pos.scalable_advance(2);
-            std.debug.print("Since your linux, you moved 2 steps, Current pos : {} \n", .{pos});
+            std.debug.print("Since your linux, you moved 2 steps, Current pos : {}\n", .{pos.current_pos});
         },
         .macos => {
             pos.reset();
             pos.scalable_advance(10);
-            std.debug.print("Since your Mac, you moved 10 steps, Current pos : {} \n", .{pos});
+            std.debug.print("Since your Mac, you moved 10 steps, Current pos : {}\n", .{pos.current_pos});
         },
         else => {
-            std.debug.print("Unknown os , failed to proceed.\n", .{});
+            std.debug.print("Unknown os, failed to proceed.\n", .{});
         },
     }
 }
