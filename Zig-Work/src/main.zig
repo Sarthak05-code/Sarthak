69c8179d36167e10e621ea667553363c0ec0c993
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-29 15:43:30 +0545

    fuck

--- Zig-Work/src/main.zig
+++ Zig-Work/src/main.zig
@@ -1,43 +1,16 @@
 const std = @import("std");
+const built = @import("builtin");
+const os = built.os.tag;
 
-const GLOBAL : i32 = 1;
+pub fn main() void {
+    if (os == .windows) {
+        std.debug.print("The os is windows. \n", .{});
+    } else {
+        std.debug.print("The os is known: \n", .{});
+    }
 
-
-pub const Outer = struct {
-    pub const Inner = struct {
-        value: i32 = undefined,
-        is_active: bool = false,
-
-        const InnerError = error{NotActive};
-
-        pub fn init(self: *Inner) void {
-            self.value = 0;
-            self.is_active = true;
-        }
-
-        pub fn return_value(self: *const Inner) InnerError!i32 {
-            if (!self.is_active) {
-                return error.NotActive;
-            }
-            return self.value;
-        }
-    };
-};
-
-pub fn main() !void {
-    std.debug.print("This should work. \n", .{});
-    std.debug.print("This\tname\twill\thave\tspace", .{});
-
-    var tester: Outer.Inner = .{};
-
-    const before = tester.return_value() catch |err| blk: {
-        std.debug.print("Not ready yet: {}\n", .{err});
-        break :blk -1;
-    };
-    std.debug.print("Value is: {}\n", .{before});
-
-    tester.init();
-
-    const after = try tester.return_value();
-    std.debug.print("Value is: {}\n", .{after});
+    var i : i32 = 0;
+    while (i < 10) : (i += 1) {
+        std.debug.print("{}\n", .{i + 1});
+    }
 }
