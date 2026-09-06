b5f47df7a3e9e2cf3d44718cf240975b9d99fde2
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-09-06 18:35:10 +0545

    removed after testing

--- Zig-Work/src/Field.zig
+++ Zig-Work/src/Field.zig
@@ -1,9 +1,4 @@
 const std = @import("std");
-const heap = std.heap;
-const allocator = heap.BrkAllocator;
-const built = @import("builtin");
-const os = built.os.tag;
-
 
 pub const Name = struct {
     first_name: []const u8,
