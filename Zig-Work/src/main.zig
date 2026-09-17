a2711aa289e1ca4fcd8caa19232b07b777bfcdae
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-09-17 15:50:10 +0545

    login value

--- Zig-Work/src/main.zig
+++ Zig-Work/src/main.zig
@@ -1,6 +1,22 @@
 const std = @import("std");
 
+const Login = struct { sucess: bool, client_id: i32, role: []const u8 };
+
+fn login(email: []const u8, password: []const u8) !Login {
+    _ = password;
+
+    if (std.mem.eql(u8, email, "ytsarthak1@gmail.com")) {
+        return .{ .sucess = true, .client_id = 101, .role = "client" };
+    }
+    return .{ .sucess = false, .client_id = 0, .role = "guest" };
+}
+
 pub fn main(init: std.process.Init) !void {
     _ = init;
-    
+    const value = try login("ytsarthak1@gmail.com", "123456sarthak");
+    if (value.sucess) {
+        std.debug.print("Login sucessfull. Hello, {s}\n", .{value.role});
+    } else {
+        std.debug.print("Login unsuccessfull, hello {s}\n", .{value.role});
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,12 @@
-#include <cstdint>
-#include <iomanip>
 #include <iostream>
-typedef uint64_t i64;
 
-int returnValue(int number_one, int number_two, int value = 1) {
-  if (value != 1 && value != 0) {
-    std::cout << "The number you entered wasn't a proper value" << "\n";
-    return 1;
-  }
-  if (value == 1)
-    return number_one > number_two ? number_one : number_one;
-  return number_one > number_two ? number_two : number_one;
-}
+using namespace std;
 
-template <typename T> T findArea(T a, T b) { return a * b; }
+constexpr int number = 10;
 
-int main() {
-  int num = 10, ber = 20;
-  std::cout << "The number is : " << returnValue(num, ber);
-  std::cout << "The number is : " << returnValue(num, ber, 1);
+int main(void) {
+
+  cout << "Stuffs" << endl;
 
   return 0;
 }
