2bd9a82a080e1aa5ad03ecd7fb60088e751a1d55
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-09-17 17:30:35 +0545

    feat , added a more auth login

--- Zig-Work/src/main.zig
+++ Zig-Work/src/main.zig
@@ -1,22 +1,108 @@
 const std = @import("std");
 
-const Login = struct { sucess: bool, client_id: i32, role: []const u8 };
+const Invalid = error{ EmptyValues, InvalidUserLogin, SamePassword };
 
-fn login(email: []const u8, password: []const u8) !Login {
-    _ = password;
+var ID: i32 = 100;
 
-    if (std.mem.eql(u8, email, "ytsarthak1@gmail.com")) {
-        return .{ .sucess = true, .client_id = 101, .role = "client" };
-    }
-    return .{ .sucess = false, .client_id = 0, .role = "guest" };
+const Register = struct {
+    username: []const u8,
+    password: []const u8,
+    email: []const u8,
+    client_id: i32,
+};
+
+const Login = struct {
+    email: []const u8,
+    password: []const u8,
+};
+
+fn createUser(
+    username: ?[]const u8,
+    password: []const u8,
+    email: []const u8,
+) Invalid!Register {
+    const set_username = username orelse "defaultuser123";
+
+    if (password.len == 0 or email.len == 0)
+        return Invalid.EmptyValues;
+
+    const user = Register{
+        .client_id = ID,
+        .username = set_username,
+        .password = password,
+        .email = email,
+    };
+
+    ID += 1;
+
+    return user;
 }
 
-pub fn main(init: std.process.Init) !void {
-    _ = init;
-    const value = try login("ytsarthak1@gmail.com", "123456sarthak");
-    if (value.sucess) {
-        std.debug.print("Login sucessfull. Hello, {s}\n", .{value.role});
-    } else {
-        std.debug.print("Login unsuccessfull, hello {s}\n", .{value.role});
+fn showUser(users: []const Register, client_id: i32) Invalid!Login {
+    for (users) |user| {
+        if (user.client_id == client_id) {
+            return .{
+                .email = user.email,
+                .password = user.password,
+            };
+        }
     }
+
+    return Invalid.InvalidUserLogin;
+}
+
+fn changePassword(
+    new_password: []const u8,
+    client_id: i32,
+    users: []Register,
+) !void {
+    if (new_password.len == 0) {
+        return Invalid.EmptyValues;
+    }
+
+    for (users) |*user| {
+        if (user.client_id == client_id) {
+            if (std.mem.eql(u8, user.password, new_password)) {
+                return Invalid.SamePassword;
+            }
+
+            user.password = new_password;
+            return;
+        }
+    }
+
+    return Invalid.InvalidUserLogin;
+}
+
+pub fn main() !void {
+    var users: [3]Register = undefined;
+
+    users[0] = try createUser(
+        "Sarthak",
+        "pass123",
+        "sarthak@example.com",
+    );
+
+    users[1] = try createUser(
+        "Alice",
+        "alice123",
+        "alice@example.com",
+    );
+
+    users[2] = try createUser(
+        "Bob",
+        "bob123",
+        "bob@example.com",
+    );
+
+    const login = try showUser(users[0..], 101);
+
+    std.debug.print(
+        "Email: {s}\nPassword: {s}\n",
+        .{ login.email, login.password },
+    );
+
+    try changePassword("bobber123", 102, &users);
+    const bob_login = try showUser(users[0..], 102);
+    std.debug.print("Email : {s} | Password : {s}\n", .{ bob_login.email, bob_login.password });
 }
