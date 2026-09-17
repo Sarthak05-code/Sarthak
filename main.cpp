4b26ae0872a6a3c94df2d16b43a6b1855aa6dfa7
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-09-17 18:44:46 +0545

    char to name converter and vice versa

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
+#include "types.hpp"
+
+#include <cstddef>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-constexpr int number = 10;
+constexpr int numbers = 10;
 
 int main(void) {
 
-  cout << "Stuffs" << endl;
+  float a = 10.0, b = 22.0;
+  float result = returnNumber(a, b);
+  cout << "The result is : " << result << endl;
+  vector<int> characters = {96, 98, 123, 234, 121, 55, 77, 86, 33, 50};
+
+  for (int character : characters) {
+    cout << character << "\t | \t" << namecaller(character) << "\n";
+  }
+  cout << "\n";
+
+  string name = "Sarthak Thapa";
+  int length = name.length();
+
+  for (size_t i = 0; i < length; ++i) {
+    cout << name[i] << "\t|\t" << static_cast<int>(name[i])
+         << "\n"; // (int)name[i] older version of typecasting;
+  }
 
   return 0;
 }
---
+++ types.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+template <typename T> T returnNumber(const T &a, const T &b) { return a + b; }
+
+static inline char namecaller(int character) {
+  return static_cast<char>(character);
+}
